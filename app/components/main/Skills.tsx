40ad0c85ec820ad8e147b12c9bf3b6e19f4b0384
lucioschiavoni <luciosc1798@gmail.com>
2024-05-16 12:29:34 -0300

    animated icons

--- app/components/main/Skills.tsx
+++ app/components/main/Skills.tsx
@@ -1,14 +1,17 @@
 import { skills_data } from "@/app/json/skills"
+import SkillCard from "../sub/SkillCard"
 
 const Skills = () => {
   return (
     <div className='text-2xl text-white z-[20]  justify-center mt-24 grid grid-cols-3 gap-10 w-6/12 m-auto' id="skills">
         {
         skills_data.map((item, index) => (
-            <div key={index} className="backdrop-blur py-4 button-primary text-center text-white cursor-pointer max-w-[200px] font-semibold  gap-5 flex flex-col justify-center items-center hover:scale-110 transition-all delay-150 duration-300  p-6 rounded-md shadow-xl">
-                <img src={item.image} alt="img-skills" className="" width={100} />
-                <p className="text-center">{item.name}</p>
-            </div>
+            <SkillCard 
+            src={item.image}
+            index={index}
+            width={100}
+            height={100}
+            />
         ))
     }</div>
   )
